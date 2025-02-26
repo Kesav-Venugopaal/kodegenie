2891c0b5da2cf57dd84d31a6bfe8a5d5f171c185
Chetna Shekhawat <153760374+Chetnashekhawat09@users.noreply.github.com>
2025-02-26 21:41:53 +0530

    Backend Updates

---
+++ code_converter/code_converter/urls.py
@@ -0,0 +1,8 @@
+# code_converter/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('converter.urls')),
+]
